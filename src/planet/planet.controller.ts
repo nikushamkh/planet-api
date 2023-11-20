967db6fc437c4cad0ccea35676ebea6c39686890
nikushamkh <nikusha.mkh@gmail.com>
2023-11-20 13:08:50 +0400

    second commit

--- src/planet/planet.controller.ts
+++ src/planet/planet.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Param } from "@nestjs/common";
-import { PlanetService } from "./planet.service";
+import { Controller, Get, Param } from '@nestjs/common';
+import { PlanetService } from './planet.service';
 
 @Controller('planets')
 export class PlanetController {
@@ -7,6 +7,11 @@ export class PlanetController {
 
   @Get(':name')
   async getPlanetByName(@Param('name') name: string) {
-    return this.planetService.searchPlanets(name)
+    return this.planetService.searchPlanets(name);
+  }
+
+  @Get('name')
+  async getSatteliteByName(@Param('name') planetId: number) {
+    return this.planetService.getPlanetSatellites(planetId);
   }
 }
--- src/planet/planet.service.ts
+++ src/planet/planet.service.ts
@@ -15,7 +15,6 @@ export class PlanetService {
     };
     const options = {
       outFormat: oracledb.OUT_FORMAT_OBJECT,
-      // Add any other options you need
     };
 
     const result = await this.oracleDBService.execute<Record<string, any>>(
@@ -32,8 +31,51 @@ export class PlanetService {
     return rows;
   }
 
+  async getPlanetSatellites(planetId: number) {
+    const sql = `BEGIN :result := get_planet_satellites(:planetId); END;`;
+    const bindParams = {
+      result: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
+      planetId,
+    };
+    const options = {
+      outFormat: oracledb.OUT_FORMAT_OBJECT,
+    };
 
+    const result = await this.oracleDBService.execute<Record<string, any>>(
+      sql,
+      bindParams,
+      options,
+    );
 
+    const resultSet = result.outBinds.result;
+    const rows = await resultSet.getRows();
 
-  
+    await resultSet.close();
+
+    return rows;
+  }
+
+  async getPlanetAtmospheres(planetId: number) {
+    const sql = `BEGIN :result := get_planet_atmospheres(:planetId); END;`;
+    const bindParams = {
+      result: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
+      planetId,
+    };
+    const options = {
+      outFormat: oracledb.OUT_FORMAT_OBJECT,
+    };
+
+    const result = await this.oracleDBService.execute<Record<string, any>>(
+      sql,
+      bindParams,
+      options,
+    );
+
+    const resultSet = result.outBinds.result;
+    const rows = await resultSet.getRows();
+
+    await resultSet.close();
+
+    return rows;
+  }
 }
